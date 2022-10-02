201dc6dcd4a27fea07fbdffb0186e135dc3cd4c0
Marc <marc@MacBook-Pro-7.local>
2022-10-02 19:19:52 -0400

    fixing print logic for network confirmation. cleaned up some of the confirmation prints

--- provisioner.py
+++ provisioner.py
@@ -9,8 +9,6 @@ import time
 config = configparser.ConfigParser()
 config.read('config')
 
-
-
 def request(handler): 
     """ This request funuction accepts handler as its passed parameter and
     should contain the resource location to build the URL.
@@ -35,47 +33,45 @@ def get_prefix(response):
         network.append(octet)
     return network
 
-def tag():
+def set_tag():
   print('entering tagging function')
   handler = "ip-addresses/{}".format(ipid)
   url, headers = request(handler)
   ipdict = {"address": "{}/{}".format(hip, prefix)}
-  response = requests.post(url, json=ipdict, verify=False, headers=headers)
+  response = requests.patch(url, json=ipdict, verify=False, headers=headers)
   print(response)
 
 def add_ip(hip, prefix):
-  print('entering add IP function')
   handler = "ip-addresses/"
   url, headers = request(handler)
   ipdict = {"address": "{}/{}".format(hip, prefix)}
   response = requests.post(url, json=ipdict, verify=False, headers=headers)
   print(response)
+  blah = input("added IP:".format(hip))
 
 def dns_update(hip, ipid):
     handler = "ip-addresses/{}/".format(ipid)
     url, headers = request(handler)
     try:
-        print('adding dns record for {}'.format(hip))
         dns_name = socket.gethostbyaddr(hip)
         print(dns_name[0])
         dnsdict = {"dns_name": "{}".format(dns_name[0])}
         response = requests.patch(url, json=dnsdict, verify=False, headers=headers)
-        print(response)
+        print('adding dns record for {}'.format(hip))
+        #print(response)
     except:
-        pass
+        print('PRT record missing for {}. Please check zone'.format(hip))
 
 def exist_check(ip_check_dict, hip, prefix):
   handler = 'ip-addresses/?limit=5000'
   if not ip_check_dict:
     url, headers = request(handler)
-    a = input('making API call')
     response = requests.get(url, verify=False, headers=headers)
     ip_check_dict = response.json()
   for key, val in enumerate(ip_check_dict['results']):
     _network = val["display"].split("/")
     ipid = val['id']
     if _network[0] == hip:
-      print('ip exists, skipping')
       dns_update(hip, ipid)
       return True, ip_check_dict
     else:
@@ -101,13 +97,12 @@ def ip_check_create():
         sortednet = items.split('/')
         items = 1
         total = 0
+        print('*'*25)
+        print('testing network: {}'.format(sortednet[0]))
         for key, val in enumerate(response['results']):
-            print('*'*25)
-            print('testing network: {}'.format(sortednet[0]))
             while items != 255:
                 hip = '{}{}'.format(network, items)
                 exists, ip_check_dict = exist_check(ip_check_dict, hip, sortednet[1])
-                print('pinging: ', hip )
                 #host = ping(hip, count=1, interval=0.01, timeout=0.1, privileged=False)
                 host = ip_check(hip)
                 if exists is True:
@@ -115,11 +110,11 @@ def ip_check_create():
                 if host.is_alive ==  True and exists is False:
                   add_ip(hip, sortednet[1])
                 elif host.is_alive == False and exists is True:
-                    print('{} is dead but is in the database'.format(hip))
+                    print('{} is offline but is in the database'.format(hip))
                 else:
                     pass  
                 items = items + 1
-        print('Total IPs Alive: ', total)
+        print('Total IPs active and in the database: {}'.format(total))
 
     
 ip_check_create()
